883570392bbdc8e2382db3bd1e1f52bdd69394cc
th174@duke.edu <th174@duke.edu>
2017-03-30 19:28:49 -0400

    Streamified a lot of stuff, added grid constructor, refactored unit package

--- src/backend/grid/BoundaryConditions.java
+++ src/backend/grid/BoundaryConditions.java
@@ -11,18 +11,11 @@ import java.util.stream.IntStream;
 @FunctionalInterface
 public interface BoundaryConditions {
     BoundaryConditions INFINITEBOUNDS = (grid, input) -> input;
-    BoundaryConditions FINITEBOUNDS = (grid, input) -> new CoordinateTuple(
-            IntStream.range(0, input.dimension())
-                    .map(i -> Math.min(Math.max(input.get(i), grid.getRectangularBounds().getMax(i)), grid.getRectangularBounds().getMin(i))).boxed()
-                    .collect(Collectors.toList()));
-    BoundaryConditions TOROIDALBOUNDS = (grid, input) -> new CoordinateTuple(
-            IntStream.range(0, input.dimension())
-                    .map(i -> {
-                        int range = grid.getRectangularBounds().getMax(i) - grid.getRectangularBounds().getMin(i);
-                        return (input.get(i) - grid.getRectangularBounds().getMin(i) + range * 1000) % range + grid.getRectangularBounds().getMin(i);
-                    }).boxed()
-                    .collect(Collectors.toList()));
-
+    BoundaryConditions FINITEBOUNDS = (grid, input) -> new CoordinateTuple(IntStream.range(0, input.dimension()).map(i -> Math.min(Math.max(input.get(i), grid.getRectangularBounds().getMax(i)), grid.getRectangularBounds().getMin(i))).boxed().collect(Collectors.toList()));
+    BoundaryConditions TOROIDALBOUNDS = (grid, input) -> new CoordinateTuple(IntStream.range(0, input.dimension()).map(i -> {
+        int range = grid.getRectangularBounds().getMax(i) - grid.getRectangularBounds().getMin(i);
+        return (input.get(i) - grid.getRectangularBounds().getMin(i) + range * 1000) % range + grid.getRectangularBounds().getMin(i);
+    }).boxed().collect(Collectors.toList()));
 
     CoordinateTuple getMappedCoordinate(Grid grid, CoordinateTuple input);
 }
