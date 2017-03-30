50f3fb897146195df7c1ba1196cff94ce4074273
Stone Mathers <smm117@duke.edu>
2017-03-29 23:28:32 -0400

    Added methods to CollapsibleList

--- src/frontend/CollapsibleList.java
+++ src/frontend/CollapsibleList.java
@@ -3,10 +3,17 @@
  */
 package frontend;
 
+import java.util.List;
+
 /**
- * @author Stone Mathers
- * Created 3/29/2017
+ * @author Stone Mathers Created 3/29/2017
  */
 public interface CollapsibleList extends Displayable {
+
+	void collapse();
+
+	List<ListItem> getItems();
+
+	void addItem(ListItem item);
 
 }
