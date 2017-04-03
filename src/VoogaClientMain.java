62dfc90f2db56794893ece2f52db6f5f6114220e
th174@duke.edu <th174@duke.edu>
2017-04-03 05:10:18 -0400

    fixed bug with lambdas

--- src/VoogaClientMain.java
+++ src/VoogaClientMain.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * @author Created by th174 on 4/1/2017.
  */
 public class VoogaClientMain {
-    public static final String HOSTNAME = "25.13.30.96";
+    public static final String HOSTNAME = "25.4.129.184";
     //    public static final String HOSTNAME = Client.LOCALHOST;
 
     public static void main(String[] args) throws Exception {
@@ -16,11 +16,12 @@ public class VoogaClientMain {
                 SimpleChatLogTest::toString,
                 obj -> (SimpleChatLogTest) Class.forName(obj.toString().split("=")[0]).getConstructor(String.class).newInstance(obj.toString().split("=")[1]));
         Scanner stdin = new Scanner(System.in);
-        voogaClient.addListener(state -> System.out.print(state.getLast() + "\n\n>>  "));
+        voogaClient.addListener(state -> System.out.print("\n" + state.getLast() + "\n\n>>  "));
         voogaClient.start();
         while (voogaClient.isActive()) {
             String input = stdin.nextLine();
-            voogaClient.send(state -> state.appendMessage(input, System.getProperty("user.name")));
+            String user = System.getProperty("user.name");
+            voogaClient.send(state -> state.appendMessage(input, user));
         }
     }
 }
