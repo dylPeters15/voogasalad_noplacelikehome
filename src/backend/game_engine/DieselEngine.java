a7e4b527551cf0de5ca60a1e857220545647b5b1
th174@duke.edu <th174@duke.edu>
2017-04-03 23:25:28 -0400

    fixed compile error

--- src/backend/game_engine/DieselEngine.java
+++ src/backend/game_engine/DieselEngine.java
@@ -1,55 +1,55 @@
 package backend.game_engine;
 
-import java.util.function.Consumer;
-
 import backend.player.Player;
 import backend.util.GameState;
-import util.net.Server;
+import util.net.ObservableServer;
 
-public class DieselEngine implements GameEngine{
+import java.util.function.Consumer;
 
-	private Server server;
-	private Consumer<GameState> stateUpdateListener = this::checkRules;
-	
-	public DieselEngine(Server s){
-		server = s;
-		//server.addListener(stateUpdateListener);
-		server.start();
-	}
-	
-	private void checkRules(GameState state){
-		
-		return;
-	}
+public class DieselEngine implements GameEngine {
 
-	@Override
-	public void restart() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public void save() {
-		// TODO Auto-generated method stub
-		
-	}
+    private ObservableServer<GameState> server;
+    private Consumer<GameState> stateUpdateListener = this::checkRules;
 
-	@Override
-	public void load() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public void messagePlayer(Player from, Player to, String message) {
-		// TODO Auto-generated method stub
-		
-	}
+    public DieselEngine(ObservableServer<GameState> s) {
+        server = s;
+        //server.addListener(stateUpdateListener);
+        server.start();
+    }
 
-	@Override
-	public String toXML() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private void checkRules(GameState state) {
+
+        return;
+    }
+
+    @Override
+    public void restart() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void save() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void load() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void messagePlayer(Player from, Player to, String message) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String toXML() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
