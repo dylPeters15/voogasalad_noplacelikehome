30c923943b754aaaaa95afc51fb775aca7f29b87
Faith Rodriguez <far10@duke.edu>
2017-04-03 23:10:46 -0400

    moving to right packages

--- src/frontend/templatepane/TemplatePane.java
+++ src/frontend/templatepane/TemplatePane.java
@@ -5,6 +5,10 @@ import javafx.scene.layout.Region;
 
 public class TemplatePane extends BaseUIManager<Region>{
 
+	public TemplatePane() {
+		SpriteAddPane spritepane = new SpriteAddPane();
+	}
+	
 	@Override
 	public Region getObject() {
 		// TODO Auto-generated method stub
