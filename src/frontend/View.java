b657125df204af0b440d07323d628f5910abdc26
th174@duke.edu <th174@duke.edu>
2017-04-03 23:32:44 -0400

    fixed compile error, again. please make sure code compiles before pushing to master, else use a different branch

--- src/frontend/View.java
+++ src/frontend/View.java
@@ -1,42 +1,46 @@
 /**
- * 
+ *
  */
 package frontend;
 
-import javafx.scene.Node;
 import backend.util.ImmutableGameState;
+import controller.Controller;
+import frontend.detailpane.DetailPane;
+import frontend.templatepane.TemplatePane;
+import frontend.toolspane.ToolsPane;
+import frontend.voogamenubar.VoogaMenuBar;
+import frontend.worldview.WorldView;
 
 /**
  * @author Stone Mathers, Dylan Peters
- * Created 4/3/2017
+ *         Created 4/3/2017
  */
 public class View extends BaseUIManager {
 
-	private VoogaMenuBar menuBar;
-	private WorldView worldView;
-	private ToolsPane toolsPane;
-	private DetailPane infoPane;
-	private TemplatePane tempPane;
-	
-	
-	public View(ImmutableGameState gameState, ClientController controller){
-		
-	}
-	
-	
-	
-	public void update(){
-		
-	}
-	
-	private void instantiateListeners(){
-		
-	}
+    private VoogaMenuBar menuBar;
+    private WorldView worldView;
+    private ToolsPane toolsPane;
+    private DetailPane infoPane;
+    private TemplatePane tempPane;
 
-	@Override
-	public Object getObject() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+    public View(ImmutableGameState gameState, Controller controller) {
+
+    }
+
+
+    public void update() {
+
+    }
+
+    private void instantiateListeners() {
+
+    }
+
+    @Override
+    public Object getObject() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
