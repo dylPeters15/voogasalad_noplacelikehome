185b98761c390f4359fbfeff24c801eba426807e
Faith Rodriguez <far10@duke.edu>
2017-04-03 22:38:42 -0400

    Added dragging feature to sprite. Needs to be implemented from game grid

---
+++ src/frontend/SpriteAddPane.java
@@ -0,0 +1,64 @@
+package frontend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import frontend.sprites.Sprite;
+import frontend.sprites.SpriteCell;
+import frontend.sprites.Unit;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+
+public class SpriteAddPane implements Displayable {
+
+	FlowPane parent = new FlowPane();
+	Unit unit;
+	List<Sprite> availableSprites;
+	
+	public SpriteAddPane() {
+		unit = new Unit();
+		updateSprites();
+		parent.setMaxWidth(100);
+		parent.setOrientation(Orientation.HORIZONTAL);
+		
+	}
+
+	private void updateSprites() {
+		availableSprites = new ArrayList<Sprite>();
+		parent.getChildren().clear();
+		for (Sprite sprite: availableSprites) {
+			ImageView spriteImage = new ImageView(sprite.getImage());
+			parent.getChildren().add(spriteImage);
+			 sprite.setOnDragDetected(new EventHandler <MouseEvent>() {
+		            public void handle(MouseEvent event) {
+		                /* drag was detected, start drag-and-drop gesture*/
+		                System.out.println("onDragDetected");
+		                
+		                /* create dragboard */
+		                Dragboard db = (Dragboard) Dragboard.getSystemClipboard();
+		                
+		                /* put an image on dragboard */
+		                ClipboardContent content = new ClipboardContent();
+		                content.putImage(sprite.getImage());
+		                db.setContent(content);
+		                event.consume();
+		            }
+		        });
+		}
+		
+	}
+
+	@Override
+	public Node getView() {
+		return parent;
+	}
+}
