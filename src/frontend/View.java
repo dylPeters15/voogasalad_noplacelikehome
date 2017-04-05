f1df9858c609319d8568c4713179ac7d0c45dd1e
Stone Mathers <smm117@duke.edu>
2017-04-05 00:51:57 -0400

    View BorderPane init, can enter Dev and Play modes, ToolsPane can update sprites

--- src/frontend/View.java
+++ src/frontend/View.java
@@ -10,17 +10,20 @@ import frontend.templatepane.TemplatePane;
 import frontend.toolspane.ToolsPane;
 import frontend.voogamenubar.VoogaMenuBar;
 import frontend.worldview.WorldView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
 
 /**
  * @author Stone Mathers, Dylan Peters
  *         Created 4/3/2017
  */
-public class View extends BaseUIManager {
+public class View extends BaseUIManager<Region> {
 
+	private BorderPane myBorder;
 	private VoogaMenuBar menuBar;
 	private WorldView worldView;
 	private ToolsPane toolsPane;
-	private DetailPane infoPane;
+	private DetailPane detailPane;
 	private TemplatePane tempPane;
 	private ImmutableGameState myGameState;
 	private ClientController myController;
@@ -30,7 +33,7 @@ public class View extends BaseUIManager {
 	public View(ImmutableGameState gameState, ClientController controller){
 		myGameState = gameState;
 		myController = controller;
-		initPanesAndListeners();
+		initBorderPane();
 	}
 	
 	
@@ -40,18 +43,65 @@ public class View extends BaseUIManager {
 	 */
 	public void update(){
 		worldView.updateGrid(myGameState.getGrid());
+		//TODO: something like templatePane.updateSprites(myGameState.getUnits());
 	}
 	
 	/**
-	 * Instantiates all panes in the GUI and makes View a listener to all necessary panes.
+	 * Performs all necessary actions to convert the View into development mode.
+	 * If the View is already in development mode, then nothing visually changes.
+	 */
+	public void enterDevMode(){
+		addSidePanes();
+	}
+	
+	/**
+	 * Performs all necessary actions to convert the View into play mode.
+	 * If the View is already in play mode, then nothing visually changes.
+	 */
+	public void enterPlayMode(){
+		removeSidePanes();
+	}
+	
+	private void initBorderPane(){
+		initPanesAndListeners();
+		myBorder = new BorderPane(worldView.getObject(), menuBar.getObject(), tempPane.getObject(), 
+									detailPane.getObject(), toolsPane.getObject());
+	}
+	
+	/**
+	 * Initializes all panes in the GUI and makes View a listener to all necessary panes.
 	 */
 	private void initPanesAndListeners(){
-		
+		menuBar = new VoogaMenuBar();
+		//TODO: make View a listener of menuBar
+		worldView = new WorldView();
+		//TODO: make View a listener of worldView
+		//TODO: toolsPane = new ToolsPane(myGameState.getUnits());
+		//TODO: make View a listener of toolsPane
+		detailPane = new DetailPane();
+		//TODO: make View a listener of detailPane
+		tempPane = new TemplatePane();
+		//TODO: make View a listener of tempPane
+	}
+	
+	/**
+	 * Adds the ToolsPane and TemplatePane to the sides of the View's GUI.
+	 */
+	private void addSidePanes(){
+		myBorder.setLeft(toolsPane.getObject());
+		myBorder.setRight(tempPane.getObject());
+	}
+	
+	/**
+	 * Removes the ToolsPane and TemplatePane from the sides of the View's GUI.
+	 */
+	private void removeSidePanes(){
+		myBorder.setLeft(null);
+		myBorder.setRight(null);
 	}
 
 	@Override
-	public Object getObject() {
-		// TODO Auto-generated method stub
-		return null;
+	public Region getObject() {
+		return myBorder;
 	}
 }
