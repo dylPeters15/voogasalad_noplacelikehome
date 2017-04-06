fdb4d88843c44c76f11a56e6593767ebc73ca4ad
Alexander Zapata <az73@duke.edu>
2017-04-06 19:14:52 -0400

    API Review stuff for lab.

--- src/input_parse/ParseMain.java
+++ src/input_parse/ParseMain.java
@@ -8,7 +8,7 @@ public class ParseMain {
 	public static void main(String[] args) {
 		GameState gameState = new TestGameState();
 		Parser parser = new Parser(gameState);
-		parser.parse(Type.CONSUMER, "(hello) -> {hello.endTurn();};");
+		parser.parse(Type.PREDICATE, "(hello) -> {hello.endTurn(); return true;};");
 		UserConsumer consumer = new UserConsumer();
 		consumer.getConsumer().accept(gameState);
 	}
