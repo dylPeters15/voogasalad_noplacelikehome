56f6b1415bcfc18a814ed8f9e599aa145b68e512
Andreas Santos <ajs118@duke.edu>
2017-04-08 01:11:04 -0400

    more functionality of ImageNamePairView and NewTerrainPain

--- src/frontend/newterrainpane/NewTerrainPane.java
+++ src/frontend/newterrainpane/NewTerrainPane.java
@@ -1,14 +1,39 @@
 package frontend.newterrainpane;
 
 import frontend.BaseUIManager;
+import frontend.util.CancelSaveView;
+import frontend.util.ImageNamePairView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 
 public class NewTerrainPane extends BaseUIManager<Region>{
+	
+	private AnchorPane myAnchorPane;
+	private CancelSaveView myCancelSaveView;
+	private ImageNamePairView myImageNamePairView;
+	
+	public NewTerrainPane(){
+		initialize();
+	}
 
+	private void initialize(){
+		myAnchorPane = new AnchorPane();
+		
+		myCancelSaveView = new CancelSaveView();
+		
+		myImageNamePairView = new ImageNamePairView();
+		
+		AnchorPane.setTopAnchor(myImageNamePairView.getObject(), 0.0);
+		AnchorPane.setLeftAnchor(myImageNamePairView.getObject(), 0.0);
+		AnchorPane.setBottomAnchor(myCancelSaveView.getObject(), 0.0);
+		AnchorPane.setRightAnchor(myCancelSaveView.getObject(), 0.0);
+		
+		myAnchorPane.getChildren().addAll(myImageNamePairView.getObject(), myCancelSaveView.getObject());
+	}
+	
 	@Override
 	public Region getObject() {
-		// TODO Auto-generated method stub
-		return null;
+		return myAnchorPane;
 	}
 
 }
