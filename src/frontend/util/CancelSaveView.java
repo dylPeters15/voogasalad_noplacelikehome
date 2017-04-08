32d13c91d193baacbbb3c37ec8c5ecd99bebe0cc
Andreas Santos <ajs118@duke.edu>
2017-04-07 21:59:27 -0400

    CancelSaveView view functionality (buttons do nothing)

--- src/frontend/util/CancelSaveView.java
+++ src/frontend/util/CancelSaveView.java
@@ -1,12 +1,47 @@
 package frontend.util;
 
 import frontend.BaseUIManager;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 
 public class CancelSaveView extends BaseUIManager<Region>{
-
 	
-	void setOnCreateNew();
-	void setOnCancel();
-	void setOnSave();
+	private HBox myCancelSaveView;
+	private Button createNew, cancel, save;
+	
+	public CancelSaveView(){
+		initialize();
+	}
+	
+	private void initialize() {
+		myCancelSaveView = new HBox();
+		createNew = new Button("Create New...");
+		createNew.setOnAction(e -> setOnCreateNew());
+		
+		cancel = new Button("Cancel");
+		cancel.setOnAction(e -> setOnCancel());
+		
+		save = new Button("Save");
+		save.setOnAction(e -> setOnSave());
+		
+		myCancelSaveView.getChildren().addAll(createNew, cancel, save);
+	}
+
+	void setOnCreateNew(){
+		
+	}
+	
+	void setOnCancel(){
+		
+	}
+	
+	void setOnSave(){
+		
+	}
+	
+	@Override
+	public Region getObject() {
+		return myCancelSaveView;
+	}
 }
