33bda5fce89ad7cff46ef4775c1d38471ac6ebf9
Andreas Santos <ajs118@duke.edu>
2017-04-08 01:58:22 -0400

    added basic functionality of numeric input row

--- src/frontend/util/NumericInputRow.java
+++ src/frontend/util/NumericInputRow.java
@@ -3,24 +3,65 @@ package frontend.util;
 import java.util.function.UnaryOperator;
 
 import frontend.BaseUIManager;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 
 public class NumericInputRow extends BaseUIManager<Region>{
-
+	
+	private HBox myNumericInputRow;
+	private TextField myNumericalInputField, myNameField, myLabelField;
+	
+	public NumericInputRow(String name, String label){
+		initialize(name, label);
+	}
+	
+	private void initialize(String name, String label){
+		myNumericInputRow = new HBox();
+		
+		myNameField = new TextField(name);
+		myNameField.setEditable(false);
+		
+		myNumericalInputField = new TextField("0");
+		
+		myLabelField = new TextField(label);
+		myLabelField.setEditable(false);
+		
+		myNumericInputRow.getChildren().addAll(myNameField, myNumericalInputField, myLabelField);
+	}
+	
 	//can implement here
 	 void setName(String name){
 		 
 	 }
-	 String getName();
-	 void setValue(Double value);
-	 double getValue();
-	 void setLabel(String label);
-	 String getLabel();
-	 void setOnValueEntry(UnaryOperator<Object> action);
-	@Override
+	 
+	 String getName(){
+		 return null;
+	 }
+	 
+	 void setValue(Double value){
+		 
+	 }
+	 
+	 double getValue(){
+		 return 0.0;
+	 }
+	 
+	 void setLabel(String label){
+		 
+	 }
+	 
+	 String getLabel(){
+		 return null;
+	 }
+	 
+	 void setOnValueEntry(UnaryOperator<Object> action){
+		 
+	 }
+	
+	 @Override
 	public Region getObject() {
-		// TODO Auto-generated method stub
-		return null;
+		return myNumericInputRow;
 	}
 	
 }
