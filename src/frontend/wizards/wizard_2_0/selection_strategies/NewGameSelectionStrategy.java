4b97bcbf46ddbb9fb9c8673015a8c3c424ac8205
Dylan Peters <dlp22@duke.edu>
2017-04-10 23:33:48 -0400

    NewGameWizard now allows user to choose cell type from a hard-coded list. Will refactor.

--- src/frontend/wizards/wizard_2_0/selection_strategies/NewGameSelectionStrategy.java
+++ src/frontend/wizards/wizard_2_0/selection_strategies/NewGameSelectionStrategy.java
@@ -1,11 +1,5 @@
 package frontend.wizards.wizard_2_0.selection_strategies;
 
-import backend.cell.Cell;
-import backend.cell.ModifiableCell;
-import backend.grid.BoundsHandler;
-import backend.grid.CoordinateTuple;
-import backend.grid.GridPattern;
-import backend.grid.ModifiableGameBoard;
 import backend.util.GameState;
 import frontend.wizards.wizard_2_0.wizard_pages.GridInstantiationPage;
 import frontend.wizards.wizard_2_0.wizard_pages.ImageNameDescriptionPage;
@@ -14,25 +8,21 @@ public class NewGameSelectionStrategy extends BaseSelectionStrategy<GameState> {
 
 	private ImageNameDescriptionPage imageNameDescriptionPage;
 	private GridInstantiationPage gridInstantiationPage;
-	
-	public NewGameSelectionStrategy(){
+
+	public NewGameSelectionStrategy() {
 		initialize();
 	}
-	
+
 	@Override
 	public GameState finish() {
-		new CoordinateTuple(1,2,3);
-		GridPattern pattern = GridPattern.HEXAGONAL_ADJACENT;
-		Cell template = ModifiableCell.BASIC_SQUARE_FLAT;
-		ModifiableGameBoard board = new ModifiableGameBoard("testBoard", template, 5, 5, BoundsHandler.TOROIDAL_BOUNDS, "", "").copy();
-		GameState gameState = new GameState(board);
+		GameState gameState = new GameState(gridInstantiationPage.getGameBoard());
 		return gameState;
 	}
-	
-	private void initialize(){
+
+	private void initialize() {
 		imageNameDescriptionPage = new ImageNameDescriptionPage();
 		gridInstantiationPage = new GridInstantiationPage();
-		getPages().addAll(imageNameDescriptionPage);//,gridInstantiationPage);
+		getPages().addAll(imageNameDescriptionPage, gridInstantiationPage);
 	}
 
 }
