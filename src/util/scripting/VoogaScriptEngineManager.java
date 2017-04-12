74e0fa0d215a5a11bf71410260e3e4db40379882
th174@duke.edu <th174@duke.edu>
2017-04-11 20:36:23 -0400

    added scripting window

--- src/util/scripting/VoogaScriptEngineManager.java
+++ src/util/scripting/VoogaScriptEngineManager.java
@@ -19,7 +19,8 @@ public final class VoogaScriptEngineManager {
 	}
 
 	public static Collection<String> getAllSupportedScriptingLanguages() {
-		return SCRIPT_ENGINE_IDENTIFIERS.values().stream().map(Class::getSimpleName).map(e -> e.replaceAll("Vooga|Engine", "")).collect(Collectors.toList());
+		initScriptEngineIdentifiers();
+		return SCRIPT_ENGINE_IDENTIFIERS.values().stream().map(Class::getSimpleName).map(e -> e.replaceAll("Vooga|Engine", "")).collect(Collectors.toSet());
 	}
 
 	public static VoogaScriptEngine read(String language, Path scriptFilePath) throws IOException {
@@ -27,17 +28,21 @@ public final class VoogaScriptEngineManager {
 	}
 
 	public static VoogaScriptEngine read(String langauge, String script) throws VoogaScriptException {
+		initScriptEngineIdentifiers();
+		try {
+			return SCRIPT_ENGINE_IDENTIFIERS.get(langauge.replace(" ", "").toLowerCase()).newInstance().setScript(script);
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new VoogaScriptException("Language not supported");
+		}
+	}
+
+	private static void initScriptEngineIdentifiers() {
 		if (SCRIPT_ENGINE_IDENTIFIERS.isEmpty()) {
 			SCRIPT_ENGINE_IDENTIFIERS.putAll(Stream.of("javascript", "js", "nashorn", "ecmascript").collect(Collectors.toMap(e -> e, e -> VoogaJavaScriptEngine.class)));
 			SCRIPT_ENGINE_IDENTIFIERS.putAll(Stream.of("groovy", "groovyshell", "apachegroovy").collect(Collectors.toMap(e -> e, e -> VoogaGroovyEngine.class)));
 			SCRIPT_ENGINE_IDENTIFIERS.putAll(Stream.of("java").collect(Collectors.toMap(e -> e, e -> VoogaJavaEngine.class)));
 			SCRIPT_ENGINE_IDENTIFIERS.putAll(Stream.of("lua", "luaj").collect(Collectors.toMap(e -> e, e -> VoogaLuaEngine.class)));
 			SCRIPT_ENGINE_IDENTIFIERS.putAll(Stream.of("jruby", "ruby").collect(Collectors.toMap(e -> e, e -> VoogaRubyEngine.class)));
-		}
-		try {
-			return SCRIPT_ENGINE_IDENTIFIERS.get(langauge.replace(" ", "").toLowerCase()).newInstance().setScript(script);
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw new VoogaScriptException("Language not supported");
 		}
 	}
 }
