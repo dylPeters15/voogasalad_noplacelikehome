3a2fd7c8f08b1a8c3bf7689270578553010a1259
Stone Mathers <smm117@duke.edu>
2017-04-11 23:03:23 -0400

    HexagonManager and Hexagon

--- src/frontend/worldview/grid/HexagonalManager.java
+++ src/frontend/worldview/grid/HexagonalManager.java
@@ -3,28 +3,27 @@
  */
 package frontend.worldview.grid;
 
-import backend.grid.CoordinateTuple;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 /**
  * @author Stone Mathers
- *
+ * Created 4/11/2017
  */
 public class HexagonalManager implements LayoutManager {
 
+	public static final int X_INDEX = 0;
+	public static final int Y_INDEX = 1;
+	
 	@Override
-	public void layoutCell(CellView cell, double scaleFactor, double min, double max) {
-		if(scaleFactor <= 0 || scaleFactor > 1 || min <= 0){
+	public void layoutCell(CellView cell, double scaleFactor, double minWidth, double maxWidth) {
+		if(scaleFactor <= 0 || scaleFactor > 1 || minWidth <= 0){
 			throw new RuntimeException();
 		}
-		
-		CoordinateTuple rectCoord = cell.getCoordinateTuple().convertToRectangular();
-		Image image = cell.getImage();
-		ImageView iv = new ImageView();
-		double length = min + ((max-min)*scaleFactor);
-		
-		
+		resizeHexagon((Hexagon)cell.getPolygon(), scaleFactor, minWidth, maxWidth);
+		double width = minWidth + ((maxWidth - minWidth) * scaleFactor);
+		cell.setX(cell.getCoordinateTuple().convertToRectangular().get(X_INDEX) * width);
+		cell.setX(cell.getCoordinateTuple().convertToRectangular().get(Y_INDEX) * width);	
 	}
-
+	
+	private void resizeHexagon(Hexagon hexagon, double scale, double min, double max){
+		hexagon.setPoints(scale, min, max);
+	}
 }
