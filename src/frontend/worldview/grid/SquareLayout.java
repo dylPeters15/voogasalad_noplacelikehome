9e0e5c77216dc2dd4fa1ba2add2d31b396425ddc
Dylan Peters <dlp22@duke.edu>
2017-04-11 23:12:09 -0400

    CellViews display

--- src/frontend/worldview/grid/SquareLayout.java
+++ src/frontend/worldview/grid/SquareLayout.java
@@ -1,56 +1,23 @@
 package frontend.worldview.grid;
 
-import java.util.Arrays;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.shape.Polygon;
-
 public class SquareLayout implements LayoutManager {
-
-	private double size;
-	private CellView cell;
-	private Double[] cellPosition;
-	private Polygon shape;
 
 	@Override
 	public void layoutCell(CellView cellIn, double scaleFactor, double min, double max) {
+		double width = (max - min) * scaleFactor + min;
+		double height = (max - min) * scaleFactor + min;
+		double xOffset = cellIn.getCoordinateTuple().get(0) * width;
+		double yOffset = cellIn.getCoordinateTuple().get(1) * height;
+		cellIn.getPolygon().getPoints().clear();
+		cellIn.getPolygon().getPoints().add(xOffset);
+		cellIn.getPolygon().getPoints().add(yOffset);
+		cellIn.getPolygon().getPoints().add(xOffset + width);
+		cellIn.getPolygon().getPoints().add(yOffset);
+		cellIn.getPolygon().getPoints().add(xOffset + width);
+		cellIn.getPolygon().getPoints().add(yOffset + height);
+		cellIn.getPolygon().getPoints().add(xOffset);
+		cellIn.getPolygon().getPoints().add(yOffset + height);
 
-//		cellIn.getPolygon().getPoints().clear();
-//		cellIn.getPolygon().getPoints().addAll(FXCollections.observableArrayList(Arrays.asList(0.0,0.0,100.0,0.0,100.0,100.0,0.0,100.0)));
-
-		cell = cellIn;
-		shape = cell.getPolygon();
-		setCoordinates();
-		setCellSize(scaleFactor, min, max);
-
-	}
-
-	private void setCellSize(double scaleFactor, double min, double max) {
-		double difference = max - min;
-		size = scaleFactor * difference + min;
-		ObservableList<Double> vertices = shape.getPoints();
-		vertices.clear();
-
-		vertices.add(cellPosition[0] - size / 2);
-		vertices.add(cellPosition[1] - size / 2);
-		vertices.add(cellPosition[0] - size / 2);
-		vertices.add(cellPosition[1] + size / 2);
-		vertices.add(cellPosition[0] + size / 2);
-		vertices.add(cellPosition[1] + size / 2);
-		vertices.add(cellPosition[0] + size / 2);
-		vertices.add(cellPosition[1] - size / 2);
-
-	}
-
-	private void setCoordinates() {
-		cellPosition = new Double[2];
-
-		Double col = (double) cell.getCoordinateTuple().get(0);
-		Double row = (double) (cell.getCoordinateTuple().get(1)
-				+ (cell.getCoordinateTuple().get(0) + (cell.getCoordinateTuple().get(0) & 1)) / 2);
-		cellPosition[0] = row;
-		cellPosition[1] = col;
 	}
 
 }
