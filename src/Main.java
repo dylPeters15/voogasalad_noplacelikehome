0237b95633aef55239e18e5f1c881b84fc5c9859
Dylan Peters <dlp22@duke.edu>
2017-04-18 14:58:19 -0400

    Replaced lines in main to fix bug

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,30 @@
+import backend.grid.GridPattern;
 import frontend.startup.StartupScreen;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    
-    public static void main(String[] args) {
-//        new Thread() {
-//            public void run() {
-//                try {
-//                    VoogaServerMain.main(args);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }.start();
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        StartupScreen starter = new StartupScreen(primaryStage, 700.0, 700.0);
-        primaryStage.setScene(starter.getPrimaryScene());
-        primaryStage.setResizable(false);
-        primaryStage.show();
-    }
+
+	public static void main(String[] args) {
+//		new Thread() {
+//			public void run() {
+//				try {
+//					VoogaServerMain.main(args);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}.start();
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		@SuppressWarnings("unused")
+		GridPattern pattern = GridPattern.HEXAGONAL_ADJACENT;
+		StartupScreen starter = new StartupScreen(primaryStage, 700.0, 700.0);
+		primaryStage.setScene(starter.getPrimaryScene());
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
 }
