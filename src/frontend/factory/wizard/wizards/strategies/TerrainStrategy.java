da15ec6bd06bdb9097c7f393d815615515154b64
Dylan Peters <dlp22@duke.edu>
2017-04-22 21:50:37 -0400

    Fixed terrain typo

--- src/frontend/factory/wizard/wizards/strategies/TerrainStrategy.java
+++ src/frontend/factory/wizard/wizards/strategies/TerrainStrategy.java
@@ -23,7 +23,7 @@ class TerrainStrategy extends BaseStrategy<Terrain> {
 	}
 
 	private void initialize(AuthoringGameState gameState) {
-		imageNameDescriptionPage = new ImageNameDescriptionPage("Create New Unit");
+		imageNameDescriptionPage = new ImageNameDescriptionPage("Create New Terrain");
 		unitMovePointPage = new UnitMovePointPage(gameState);
 		getPages().addAll(imageNameDescriptionPage, unitMovePointPage);
 	}
