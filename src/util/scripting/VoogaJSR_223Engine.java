b01a41f4997f330b13fbdc59c974abc89185c2f9
Tianyue Huang <th174@duke.edu>
2017-04-25 22:32:08 -0400

    scripting works

--- src/util/scripting/VoogaJSR_223Engine.java
+++ src/util/scripting/VoogaJSR_223Engine.java
@@ -2,14 +2,16 @@ package util.scripting;
 
 import javax.script.*;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Created by th174 on 4/7/2017.
  */
 public abstract class VoogaJSR_223Engine implements VoogaScriptEngine {
 	private transient static final ScriptEngineManager SCRIPT_ENGINE_MANAGER = new ScriptEngineManager();
-	private transient final ScriptEngine engine;
-	private CompiledScript compiledScript;
+	private transient ScriptEngine engine;
+	private transient CompiledScript compiledScript;
+	private String script;
 
 	protected VoogaJSR_223Engine() {
 		engine = SCRIPT_ENGINE_MANAGER.getEngineByName(getEngineName());
@@ -20,6 +22,7 @@ public abstract class VoogaJSR_223Engine implements VoogaScriptEngine {
 	@Override
 	public VoogaJSR_223Engine setScript(String script) throws VoogaScriptException {
 		try {
+			this.script = script;
 			compiledScript = ((Compilable) engine).compile(script);
 		} catch (ScriptException e) {
 			throw new VoogaScriptException(e);
@@ -27,9 +30,17 @@ public abstract class VoogaJSR_223Engine implements VoogaScriptEngine {
 		return this;
 	}
 
+	protected final String getScript(){
+		return script;
+	}
+
 	@Override
 	public Object eval(Map<String, Object> bindings) throws VoogaScriptException {
 		try {
+			if (Objects.isNull(engine)) {
+				engine = SCRIPT_ENGINE_MANAGER.getEngineByName(getEngineName());
+				compiledScript = ((Compilable) engine).compile(getScript());
+			}
 			return compiledScript.eval(new SimpleBindings(bindings));
 		} catch (ScriptException e) {
 			throw new VoogaScriptException(e);
