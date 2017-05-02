f8c2f6c35ed8a2c3f6a5c9358868e065af60881a
Noah Pritt <ncp14@duke.edu>
2017-05-02 19:33:42 -0400

    Removed unused imports from SciptingDialogue

--- src/frontend/util/ScriptingDialog.java
+++ src/frontend/util/ScriptingDialog.java
@@ -4,42 +4,30 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Optional;
 
-import javax.imageio.ImageIO;
-
-import backend.grid.GameBoard;
 import backend.util.VoogaEntity;
 import controller.Controller;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import util.polyglot.PolyglotException;
 import scripting.VoogaScriptEngine;
 import scripting.VoogaScriptEngineManager;
 import scripting.VoogaScriptException;
 
-import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -48,7 +36,6 @@ import java.io.IOException;
 
 import frontend.factory.wizard.Wizard;
 import frontend.factory.wizard.WizardFactory;
-import frontend.factory.wizard.strategies.*;
 
 
 
