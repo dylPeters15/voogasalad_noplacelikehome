6e4a5c8369069cdd95e7615a2c5c887ee1cf55ab
Noah Pritt <ncp14@duke.edu>
2017-05-02 19:18:35 -0400

    QuickAbilityPage.java second half....

--- src/frontend/factory/wizard/strategies/wizard_pages/QuickAbilityPage.java
+++ src/frontend/factory/wizard/strategies/wizard_pages/QuickAbilityPage.java
@@ -72,5 +72,91 @@ public class QuickAbilityPage extends BaseWizardPage {
 		return cols.getValue();
 	}
 
+	private void hardcode()
+	{
+		System.out.println("got here");
+		String[] arrayOne = {getResourceBundle().getString("Enter_Damage"), getResourceBundle().getString("Enter_NumHits")};
+		String[] arrayTwo = {getResourceBundle().getString("Enter_Health"), getResourceBundle().getString("Enter_NumHealed")};
+		String[] arrayThree = {getResourceBundle().getString("Enter_Health"), getResourceBundle().getString("Enter_NumHealed")};
+
+		typesOfAbilities.put("Attacking", arrayOne);
+		typesOfAbilities.put("Defending", arrayTwo);
+		typesOfAbilities.put("Healing", arrayThree);
+		System.out.println("left here");
+	}
+
+	private void changeNames(String type)
+	{
+		String[] newValues = typesOfAbilities.get(type);
+		rows.setName(newValues[0]);
+		cols.setName(newValues[1]);
+	}
+
+	private void initialize() {
+		hardcode();
+		vbox = new VBox();
+		vbox.setAlignment(Pos.CENTER);
+		ObservableList<String> abilityNames = FXCollections.observableArrayList();
+		for (String key : typesOfAbilities.keySet())
+		{
+			abilityNames.add(key);
+		}
+		cellShapeChooser = new ComboBox<>(abilityNames);
+		cellShapeChooser.setValue(abilityNames.get(0));
+		cellShapeChooser.setOnAction(event -> changeNames(cellShapeChooser.getValue()));
+
+		Label cellShape = new Label();
+		cellShape.textProperty().bind(getPolyglot().get("Default_Ability_Type"));
+		HBox cellShapeBox = new HBox(cellShape, cellShapeChooser);
+		
+		double midDamage = (maxDamage - minDamage) /2;
+		int midNumHits = (maxHits - minHits)/2;
+
+		rows = new SliderInputRow(null, "Enter damage:");
+		rows.setMin((int) minDamage);
+		rows.setMax((int) maxDamage);
+		rows.setValue((int) midDamage);
+		cols = new SliderInputRow(null, "Enter num hits");
+		cols.setMin(minHits);
+		cols.setMax(maxHits);
+		cols.setValue(midNumHits);
+		
+		Button blankButtonOne = new Button();
+		blankButtonOne.setVisible(false);
+		Button blankButtonTwo = new Button();
+		blankButtonTwo.setVisible(false);
+		
+		Button changeDamage = new Button();
+		changeDamage.setText("Edit max/min");
+		
+		Button changeHits = new Button();
+		changeHits.setText("Edit max/min");
+		
+		HBox damageBox = new HBox();
+		damageBox.getChildren().addAll(rows.getNode(), blankButtonOne, changeDamage);
+		
+		HBox hitBox = new HBox();
+		hitBox.getChildren().addAll(cols.getNode(), blankButtonTwo, changeHits);
+		
+		vbox.getChildren().addAll(cellShapeBox, blankButtonOne, damageBox, blankButtonTwo, hitBox);
+		System.out.println("end initialize");
+		canNextWritable().setValue(true);
+	}
+	
+	public double getDamage()
+	{
+		return rows.getValue();
+	}
+	
+	public int numHits()
+	{
+		return cols.getValue();
+	}
+
+	@Override
+	public Region getNode() {
+		return vbox;
+	}
 
 }
+
